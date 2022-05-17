6131f890bd7056704c046a51b2881822e2dfc6db
Pedra Branca <alunopb@unisul.br>
2022-05-17 20:30:38 -0300

    command

---
+++ src/main/java/command/BuyStock.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package command;
+
+/**
+ *
+ * @author alunopb
+ */
+public class BuyStock implements Order {
+private Stock abcStock;
+
+    public BuyStock(Stock abcStock) {
+        this.abcStock = abcStock;
+    }
+
+    @Override
+    public void execute() {
+       abcStock.buy();
+    }
+    
+}
